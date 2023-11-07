9c1c57b6ab99865f3b3add129b132c2937d49c30
kristieg <earsy@earsy>
2023-11-08 02:06:28 +0300

    2_task

--- src/02/data.sh
+++ src/02/data.sh
@@ -3,11 +3,6 @@
 hostname=$(hostname)
 echo HOSTNAME = $hostname
 
-# Получаем текущую временную зону
 timezone=$(timedatectl show --property=Timezone --value)
-
-# Получаем смещение временной зоны относительно UTC без нулей
-offset=$(date +'%::z')
-
-# Выводим информацию о временной зоне
-echo "TIMEZONE = $timezone $offset"
+utc=$(date +'%:::z' | sed 's/+0//')
+echo "TIMEZONE = $timezone -$utc"
