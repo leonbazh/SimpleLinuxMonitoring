e1091197370fcbdba080909ea308576266bc7a1d
kristieg <earsy@earsy>
2023-11-08 22:58:16 +0300

    3_task

--- src/03/color.sh
+++ src/03/color.sh
@@ -3,65 +3,65 @@
 #first parameter
 
 if [ $1 == 1 ]; then
-  color1="\033[37" #white
+  color1="\033[37m" #white
 elif [ $1 == 2 ]; then
-  color1="\033[31" #red
+  color1="\033[31m" #red
 elif [ $1 == 3 ]; then
-  color1="\033[32" #green
+  color1="\033[32m" #green
 elif [ $1 == 4 ]; then
-  color1="\033[34" #blue
+  color1="\033[34m" #blue
 elif [ $1 == 5 ]; then
-  color1="\033[35" #purple
+  color1="\033[35m" #purple
 elif [ $1 == 6 ]; then
-  color1="\033[40" #black
+  color1="\033[40m" #black
 fi
 
 #second parameter
 
 if [ $2 == 1 ]; then
-  color2="\033[37"
+  color2="\033[37m"
 elif [ $2 == 2 ]; then
-  color2="\033[31"
+  color2="\033[31m"
 elif [ $2 == 3 ]; then
-  color2="\033[32"
+  color2="\033[32m"
 elif [ $2 == 4 ]; then
-  color2="\033[34"
+  color2="\033[34m"
 elif [ $2 == 5 ]; then
-  color2="\033[35"
+  color2="\033[35m"
 elif [ $2 == 6 ]; then
-  color2="\033[40"
+  color2="\033[40m"
 fi
 
 #third parameter
 
 if [ $3 == 1 ]; then
-  color3="\033[37"
+  color3="\033[37m"
 elif [ $3 == 2 ]; then
-  color3="\033[31"
+  color3="\033[31m"
 elif [ $3 == 3 ]; then
-  color3="\033[32"
+  color3="\033[32m"
 elif [ $3 == 4 ]; then
-  color3="\033[34"
+  color3="\033[34m"
 elif [ $3 == 5 ]; then
-  color3="\033[35"
+  color3="\033[35m"
 elif [ $3 == 6 ]; then
-  color3="\033[40"
+  color3="\033[40m"
 fi
 
 #fourth parameter
 
 if [ $4 == 1 ]; then
-  color4="\033[37"
+  color4="\033[37m"
 elif [ $4 == 2 ]; then
-  color4="\033[31"
+  color4="\033[31m"
 elif [ $4 == 3 ]; then
-  color4="\033[32"
+  color4="\033[32m"
 elif [ $4 == 4 ]; then
-  color4="\033[34"
+  color4="\033[34m"
 elif [ $4 == 5 ]; then
-  color4="\033[35"
+  color4="\033[35m"
 elif [ $4 == 6 ]; then
-  color4="\033[40"
+  color4="\033[40m"
 fi
 
 regular_text="\033[0m" #default text
